e7c88be2fa5bc97645203887ab30fb4d55957a3c
euri10 <benoit.barthelet@gmail.com>
2022-11-21 16:14:44 +0100

    Drop type before if it exists

--- src/apscheduler/datastores/asyncpg.sql
+++ src/apscheduler/datastores/asyncpg.sql
@@ -1,9 +1,11 @@
+DROP TYPE IF EXISTS coalescepolicy;
 CREATE TYPE coalescepolicy AS ENUM (
     'earliest',
     'latest',
     'all'
 );
 
+DROP TYPE IF EXISTS joboutcome;
 CREATE TYPE joboutcome AS ENUM (
     'success',
     'error',
